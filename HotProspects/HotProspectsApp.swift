43ad9d16405bc70ca8204803dac90361af2b5181
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-05-02 01:51:43 +0000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Emmanuel K. Nketia on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
